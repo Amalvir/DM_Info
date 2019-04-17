063dc3d5bbfa8b4be12ef6363b1860fb5eebfc7c
Amalvir <samalonsovirissel@gmail.com>
2019-04-17 17:22:43 +0200

    04.17 - Terminer jusqua A.2 mais courbe pas comme elle devrait l'être

---
+++ DM5/DM5_ALONSO-VIRISSEL.py
@@ -0,0 +1,50 @@
+# ALONSO-VIRISSEL Sam DM 5
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Constante :
+
+alpha = -30
+y0 = 10
+
+# A.1
+
+def Solution(t):
+    return np.exp(alpha*t)
+
+
+def F(t, y):
+    return alpha*y
+
+
+def EulerExplicite(F, a, b, y0, h):
+    t = a
+    y = y0
+    les_t = [a]
+    les_y = [y0]
+    while t + h <= b:
+        y = y + h * F(t, y)
+        t = t + h
+        les_t.append(t)
+        les_y.append(y)
+    return les_t, les_y
+
+
+def main():
+    # A.1
+    X1 = np.linspace(0, 5, 200)
+    Y1 = [Solution(t) for t in X1]
+    plt.plot(X1, Y1)
+    plt.savefig("figure_A1.pdf")
+
+    # A.2
+    plt.figure("EulerExplicite")
+    for i in [-1, -2, -3]:
+        X2, Y2 = EulerExplicite(F, 0, 5, y0, 10**i)
+        plt.plot(X2, Y2, label="pas = "+str(10**i))
+    plt.legend()
+    plt.savefig("figure_A2.pdf")
+
+    #plt.show()
+main()
